c394b68bb6a9ab5156a084f3aba107734f8fffa7
Oxtia <158207937+oxtia93@users.noreply.github.com>
2024-05-19 23:23:51 +0530

    Update Import.tsx

--- components/Settings/Import.tsx
+++ components/Settings/Import.tsx
@@ -34,26 +34,8 @@ export const Import: FC<Props> = ({ onImport }) => {
         }}
       />
 
-      <SidebarButton
-        // text={t('Upgrade to Plus')}
-        text={t('Oxtia Jailbreak Pro')}
-        icon={<IconUser size={18} />}
-        onClick={() => {
-          const element12 = document.getElementById('overlay_top');
-          const element13 = document.getElementById('setting_section_cover_premium');
-          const element1 = document.getElementById('side_popup_id');
-        // Check if the element exists and hide it
-          // if (element12 && element13 && element1) {
-          //   element12.style.display = 'block';
-          //   element13.style.display = 'block';
-          //   element1.style.display = 'none';
-           
-          // }  
-         
-      }}
       
 
-      />
     </>
   );
 
