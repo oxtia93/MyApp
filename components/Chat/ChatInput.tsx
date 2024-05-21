9c1c0eff68e776645f487ab51ad6056b4cec720a
Oxtia <158207937+oxtia93@users.noreply.github.com>
2024-05-21 11:03:53 +0530

    Update ChatInput.tsx

--- components/Chat/ChatInput.tsx
+++ components/Chat/ChatInput.tsx
@@ -437,7 +437,7 @@ export const ChatInput = ({
       <div className="px-3 pt-2 pb-3 text-center text-[12px] text-black/50 dark:text-white/50 md:px-4 md:pt-3 md:pb-6">
       
         {t(
-          "AppleGPT can make mistakes. Consider checking important information.",
+          " AppleGPT may err. Always verify crucial information independently ",
         )}
 
            {/* <a
